470f0aaf52cb116f6ae54cacb086908fb63d42d9
IceEnd <min@coolecho.net>
2017-10-24 01:35:54 +0800

    爬去仓库部分信息

--- src/spider/githubTrending.js
+++ src/spider/githubTrending.js
@@ -10,11 +10,27 @@ class GithubTrending extends Spider {
   async getData() {
     const $ = await this.getCheerio();
     const $repoList = $('.repo-list li');
+    const data = {
+      createDate: Date.parse(new Date()),
+      repositories: [],
+    };
     // const data = [];
+    const spaceReg = /\s/ig;
     $repoList.each(function () {
       const $item = $(this);
-      console.log($item.html());
+      const repoText = $item.find('.d-inline-block h3 a').text();
+      data.repositories.push({
+        author: repoText.split(' / ')[0].replace(spaceReg, ''),
+        repoName: repoText.split(' / ')[1].replace(spaceReg, ''),
+        href: $item.find('.d-inline-block h3 a').attr('href'),
+        description: $item.find('.py-1 p').text().replace(spaceReg, ''),
+        starsTotal: parseInt($item.find('.f6.text-gray.mt-2 a').eq(0).text().replace(/\s|,/ig, ''), 10),
+        forkTotal: parseInt($item.find('.f6.text-gray.mt-2 a').eq(1).text().replace(/\s|,/ig, ''), 10),
+        starsToday: parseInt($item.find('.f6.text-gray.mt-2 .d-inline-block.float-sm-right').text().replace(/\s|stars|today|,/ig, ''), 10),
+      });
     });
+    console.log(data);
+    return data;
   }
 }
 
